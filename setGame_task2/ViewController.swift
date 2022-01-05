a93d76f94f33760fa8dc7d33a77c6968250e7f10
Ahmed Abaza <aabaza32@gmail.com>
2022-01-05 09:38:32 +0200

    Initial Commit

---
+++ setGame_task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  setGame_task2
+//
+//  Created by Ahmed Abaza on 05/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
